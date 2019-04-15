8f22f7e5120a434469ca2869e9dd65a8f1bb18a4
Daniel Homann <danielhomann4@gmail.com>
2019-04-15 10:50:29 -0700

    got quiz to render

---
+++ assets/javascript/app.js
@@ -0,0 +1,65 @@
+var questions = [
+    {
+        questions: "Who played the character of Harry Potter in the movie?",
+        possiblities: ["Daniel Craig", "Matt Damon", "Chris Evans", "Daniel Red Cliff"],
+        answer: "Daniel Red Cliff"
+    },
+    {
+        questions: "What is the capital of the USA?",
+        possiblities: ["Washington D.C.", "Cleveland", "Hollywood", "Vermont"],
+        answer: "Washington D.C."
+    },
+    {
+        questions: "Who played the character of Harry Potter in the movie?",
+        possiblities: ["Daniel Craig", "Matt Damon", "Chris Evans", "Daniel Red Cliff"],
+        answer: "Daniel Red Cliff"
+    },
+    {
+        questions: "How many conitinents are there in the world?",
+        possiblities: ["5", "4", "6", "7"],
+        answer: "7"
+    },
+    {
+        questions: "How many holes are there in a full round of Golf?",
+        possiblities: ["6", "9", "15", "18"],
+        answer: "18"
+    },
+    {
+        questions: "What is the regulation height for a basketball hoop?",
+        possiblities: ["8 feet", "9 feet", "10 feet", "12 feet"],
+        answer: "10 feet"
+    },
+    {
+        questions: "What year was the first Super Bowl played?",
+        possiblities: ["1965", "1967", "1975", "1977"],
+        answer: "1967"
+    },
+    {
+        questions: "What is the highest score possible in 10 pin bowling?",
+        possiblities: ["250", "270", "300", "330"],
+        answer: "300"
+    },
+    {
+        questions: "A shuttlecock is used in what sport?",
+        possiblities: ["Baseball", "Badminton", "Cricket", "Lacrose"],
+        answer: "Badminton"
+    },
+    {
+        questions: "Who played Nuke Laloosh in Bull Durham?",
+        possiblities: ["Tim Allen", "Matt Damon", "Chris Evans", "Tim Robbins"],
+        answer: "Tim Robbins"
+    },
+
+    {
+        questions: "Which river flows through London?",
+        possiblities: ["The Thames", "The Nile", "Yellow River", "Mississippi River"],
+        answer: "The Thames"
+    }
+]
+
+$("#start").click(function(){
+    alert("hello");
+    // for(i = 0; i < questions.length; i++){
+    //     $("#game").append(questions);
+    // }
+});
